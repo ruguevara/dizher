e34665a46de4ffcd5b36354f05e7e378e04ccdee
Ru Guevara <ruslan.gr@gmail.com>
2021-07-29 09:46:35 +0500

    restored dizher.gui.__main__:main back

--- src/dizher/gui/__main__.py
+++ src/dizher/gui/__main__.py
@@ -2,7 +2,10 @@
 import sys
 import multiprocessing
 
-if __name__ == "__main__":
+
+def main():
+    # NOTE: do not remove or rename this function, it's called as a console script:
+    #       `dizher = dizher.gui.__main__:main`
     multiprocessing.freeze_support()
     multiprocessing.set_start_method('fork')
     from .app import DizherApp
@@ -10,3 +13,7 @@ if __name__ == "__main__":
     initial_fname = sys.argv[1] if len(sys.argv) > 1 else None
     app.open_image(initial_fname)
     app.event_loop()
+
+
+if __name__ == "__main__":
+    main()
