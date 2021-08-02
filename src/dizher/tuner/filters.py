c238f605a8d7811ae71b7d057e83a60e32939d40
Ru Guevara <ruslan.gr@gmail.com>
2021-08-02 18:08:56 +0500

    contrast do not touching chroma

--- src/dizher/tuner/filters.py
+++ src/dizher/tuner/filters.py
@@ -10,7 +10,7 @@ import cv2
 from skimage import img_as_float
 from skimage.exposure import adjust_sigmoid
 
-from ..converter.colors import convert_color
+from ..converter.colors import convert_color, rgb2lab, lab2rgb
 from .params import Parameter, ParamSet
 
 
@@ -111,12 +111,16 @@ class ContrastFilter(Filter):
         if self.input is None:
             return
 
+        image_lab = rgb2lab(self.input.astype(np.float32))
+        X = image_lab[..., 0] / 100
+
         if self.params.contrast > 0:
             gain = np.exp(self.params.contrast)
-
             a = (np.exp(gain/2) + 1) / (np.exp(gain/2) - 1)
-            y = (1 / (1 + np.exp(-(self.input-0.5) * gain)) - 0.5) * a + 0.5
+            Y = (1 / (1 + np.exp(-(X-0.5) * gain)) - 0.5) * a + 0.5
         else:
             a = 1 - self.params.contrast / 5
-            y = (self.input - 0.5) / (a * a) + 0.5
-        return y.clip(0., 1.)
+            Y = (X - 0.5) / (a * a) + 0.5
+        Y = np.clip(Y, 0., 1.)
+        image_lab[..., 0] = Y * 100
+        return lab2rgb(image_lab.astype(np.float32))
